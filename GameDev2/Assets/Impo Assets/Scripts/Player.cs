8f9b159adc266f298f5537eb501a835140f89a52
Alex Francoletti <alexander.francoletti@gmail.com>
2019-02-06 03:22:41 -0500

    Grappling v1

--- GameDev2/Assets/Impo Assets/Scripts/Player.cs
+++ GameDev2/Assets/Impo Assets/Scripts/Player.cs
@@ -8,16 +8,22 @@ public class Player : MonoBehaviour
     public float gravity;
     public float fast_gravity;
     public float speed = 5f;
-    public float airdashSpeed = 1.0f;
-    public float runSpeed = 8f;
+    public float airdashSpeed = 0.25f;
+    public float runSpeed = 10f;
 
 
-    public float jumpHeight = 10f;
-    public float timeToJumpApex = .4f;
+    public float jumpHeight = 8f;
+    public float timeToJumpApex = .55f;
 
     public float airdashTime = 0;
     private bool hasAirdash = false;
     private Vector3 airdashDirection = new Vector3(0, 0, 0);
+
+    private bool facingRight = true;
+
+    public bool isSwinging = false;
+    public Vector2 ropeHook;
+    public float swingForce = 4f;
 
     float jumpVelocity;
     float velocX_smooth;
@@ -43,10 +49,6 @@ public class Player : MonoBehaviour
         {
             vec = new Vector3(airdashSpeed, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            vec = new Vector3(0, airdashSpeed, 0);
-        }
         else if (Input.GetKey(KeyCode.A))
         {
             vec = new Vector3(-airdashSpeed, 0, 0);
@@ -56,6 +58,51 @@ public class Player : MonoBehaviour
             vec = new Vector3(0, airdashSpeed, 0);
         }
         return vec;
+    }
+
+    public float aimDirection()
+    {
+        float aim = 0f;
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
+        {
+            aim = 45f;
+            facingRight = true;
+        }
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            aim = 135f;
+            facingRight = false;
+        }
+        else if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+        {
+            aim = -45f;
+            facingRight = true;
+        }
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+        {
+            aim = -135f;
+            facingRight = false;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            aim = 0f;
+            facingRight = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            aim = 180f;
+            facingRight = false;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            aim = 90f;
+        }
+        if (aim == 0f)
+        {
+            if (!facingRight)
+                aim += 180f;
+        }
+        return aim;
     }
 
     Controller2D controller;
@@ -84,93 +131,130 @@ public class Player : MonoBehaviour
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         //On the ground, enable grounded only movement here
-        if (controller.cont_collision_info.below)
+        if (isSwinging)
         {
-            hasAirdash = true;
-            if (Mathf.Abs(fast_gravity) < Mathf.Abs(gravity))
+            if (directionalInput.x != 0)
             {
-                float f = fast_gravity;
-                fast_gravity = gravity;
-                gravity = f;
-            }
-            hasAirdash = true;
-            float temp;
-            //Crouch when down is pressed
-            Transform tf = this.GetComponent<Transform>();
-            if (Input.GetKey(KeyCode.S))
-            {
-                //Temp behavior
-                tf.localScale = new Vector3(5f, 2.5f, 5f);
-                speed = 0;
-                runSpeed = 0;
-            }
-            else
-            {
-                tf.localScale = new Vector3(5f, 5f, 5f);
-                speed = 5;
-                runSpeed = 10;
-            }
-            //Run when holding P
-            if (Input.GetKey(KeyCode.P))
-            {
-                if (speed < runSpeed)
-                {
-                    temp = speed;
-                    speed = runSpeed;
-                    runSpeed = temp;
-                }
-            }
-            else
-            {
-                if (speed > runSpeed)
-                {
-                    temp = speed;
-                    speed = runSpeed;
-                    runSpeed = temp;
-                }
-            }
-            //Jump when space is pressed
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                velocity.y = jumpVelocity;
-            }
-        }
-        //In the air, enable only air movement here
-        else if (!controller.cont_collision_info.below)
-        {
-            //Fastfall when down is pressed in the air
-            float temp;
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                temp = gravity;
-                gravity = fast_gravity;
-                fast_gravity = temp;
-            }
-            //Airdash when space is pressed in the air
-            if (hasAirdash && Input.GetKeyDown(KeyCode.Space))
-            {
-                //Used airdash
-                hasAirdash = false;
-                airdashTime = .3f;
-                this.airdashDirection = calculateAirdashVector();
-            }
+                //1
+                var playerToHookDirection = (ropeHook - (Vector2)transform.position).normalized;
 
-        }
+                //2
+                Vector2 perpendicularDirection;
+                if (directionalInput.x < 0)
+                {
+                    perpendicularDirection = new Vector2(-playerToHookDirection.y, playerToHookDirection.x);
+                    var leftPerpPos = (Vector2)transform.position - perpendicularDirection * -2f;
+                    Debug.DrawLine(transform.position, leftPerpPos, Color.green, 0f);
+                }
+                else
+                {
+                    perpendicularDirection = new Vector2(playerToHookDirection.y, -playerToHookDirection.x);
+                    var rightPerpPos = (Vector2)transform.position + perpendicularDirection * 2f;
+                    Debug.DrawLine(transform.position, rightPerpPos, Color.green, 0f);
+                }
 
-        if (airdashTime > 0)
-        {
-            this.GetComponent<Transform>().localPosition += this.airdashDirection;
-            airdashTime -= Time.deltaTime;
-            velocity.y = 0;
+                var force = perpendicularDirection * swingForce;
+                this.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Force);
+            }
         }
         else
         {
-            velocity.y += gravity * Time.deltaTime; //  Gravity constant
-            float targetX_velocity = directionalInput.x * speed;    //  Speed force added to horizontal velocity, no acceleration
-                                                                    //  Damping/acceleration applied throught damping.
-            velocity.x = Mathf.SmoothDamp(velocity.x, targetX_velocity, ref velocX_smooth, controller.cont_collision_info.below ? accelTime_ground : accelTime_air);
-            //  Call to move function in controller2D class
-            controller.Move(velocity * Time.deltaTime);
+            if (controller.cont_collision_info.below)
+            {
+                hasAirdash = true;
+                if (Mathf.Abs(fast_gravity) < Mathf.Abs(gravity))
+                {
+                    float f = fast_gravity;
+                    fast_gravity = gravity;
+                    gravity = f;
+                }
+                hasAirdash = true;
+                float temp;
+                //Crouch when down is pressed
+                Transform tf = this.GetComponent<Transform>();
+                if (Input.GetKey(KeyCode.S))
+                {
+                    //Temp behavior
+                    tf.localScale = new Vector3(5f, 2.5f, 5f);
+                    speed = 0;
+                    runSpeed = 0;
+                }
+                else
+                {
+                    tf.localScale = new Vector3(5f, 5f, 5f);
+                    speed = 5;
+                    runSpeed = 10;
+                }
+                //Run when holding P
+                if (Input.GetKey(KeyCode.P))
+                {
+                    if (speed < runSpeed)
+                    {
+                        temp = speed;
+                        speed = runSpeed;
+                        runSpeed = temp;
+                    }
+                }
+                else
+                {
+                    if (speed > runSpeed)
+                    {
+                        temp = speed;
+                        speed = runSpeed;
+                        runSpeed = temp;
+                    }
+                }
+                //Jump when space is pressed
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    velocity.y = jumpVelocity;
+                }
+            }
+            //In the air, enable only air movement here
+            else if (!controller.cont_collision_info.below)
+            {
+                //Fastfall when down is pressed in the air
+                float temp;
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    temp = gravity;
+                    gravity = fast_gravity;
+                    fast_gravity = temp;
+                }
+                //Airdash when space is pressed in the air
+                if (hasAirdash && Input.GetKeyDown(KeyCode.Space))
+                {
+                    //Used airdash
+                    hasAirdash = false;
+                    airdashTime = .3f;
+                    this.airdashDirection = calculateAirdashVector();
+                }
+
+            }
+
+            if (airdashTime > 0)
+            {
+                this.GetComponent<Transform>().localPosition += this.airdashDirection;
+                airdashTime -= Time.deltaTime;
+                velocity.y = 0;
+            }
+            else
+            {
+                velocity.y += gravity * Time.deltaTime; //  Gravity constant
+                float targetX_velocity = directionalInput.x * speed;    //  Speed force added to horizontal velocity, no acceleration
+                                                                        //  Damping/acceleration applied throught damping.
+                velocity.x = Mathf.SmoothDamp(velocity.x, targetX_velocity, ref velocX_smooth, controller.cont_collision_info.below ? accelTime_ground : accelTime_air);
+                //  Call to move function in controller2D class
+                controller.Move(velocity * Time.deltaTime);
+            }
+        }
+        if (velocity.x > 0)
+        {
+            facingRight = true;
+        }
+        else
+        {
+            facingRight = false;
         }
     }
 
