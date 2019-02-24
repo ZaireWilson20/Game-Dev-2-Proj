d32ab23305f316549ffaa5a3838c161ae59d495a
Zaire Wilson <Zaire.R.Wilson@gmail.com>
2019-02-24 15:40:01 -0500

    Health, Menu, Power Mode, Map, Init Dialogue

---
+++ GameDev2/Assets/HealthUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthUI : MonoBehaviour
+{
+    public GameObject[] healthSprites;
+    private int currentLife = 4;
+
+    public void loseHealth()
+    {
+        healthSprites[currentLife].SetActive(false);
+        currentLife--;
+    }
+
+
+}
