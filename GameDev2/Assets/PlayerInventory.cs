ddde9f06de181e654c8022f2c097d6b1fc0236be
Zaire Wilson <Zaire.R.Wilson@gmail.com>
2019-03-13 16:14:11 -0400

    Pick Up Init

---
+++ GameDev2/Assets/PlayerInventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+
+    private List<PickUp> completeInventory;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Pick Up")
+        {
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                PickUp tempPick = collision.gameObject.GetComponent<PickUp>();
+                completeInventory.Add(tempPick);
+                Destroy(collision.gameObject);
+
+
+            }
+        }
+    }
+
+    public void AddToInv(PickUp item)
+    {
+        PickUp tempPickUp = item;
+        completeInventory.Add(tempPickUp);
+    }
+}
---
+++ GameDev2/Assets/Scripts/Interactable Objects/PickUp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public string name;
+    public GameObject notifObj;
+    public GameObject playerRef;
+    private PlayerInventory playerInv; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(Input.GetKeyDown())
+            notifObj.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            notifObj.SetActive(false);
+        }
+    }
+}
