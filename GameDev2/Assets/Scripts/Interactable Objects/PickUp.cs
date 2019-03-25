4af23ab69b3ba40d70d441edc8844bbcacfb4245
Zaire Wilson <Zaire.R.Wilson@gmail.com>
2019-03-24 20:33:32 -0400

    Added Script for Going thru doors with or without keys

--- GameDev2/Assets/Scripts/Interactable Objects/PickUp.cs
+++ GameDev2/Assets/Scripts/Interactable Objects/PickUp.cs
@@ -9,6 +9,8 @@ public class PickUp : MonoBehaviour
     public GameObject playerRef;
     private PlayerInventory playerInv;
     private bool playerIn; 
+
+    
     // Start is called before the first frame update
     void Start()
     {
