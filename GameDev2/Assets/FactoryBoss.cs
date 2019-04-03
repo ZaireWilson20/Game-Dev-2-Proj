015d7a591f0dd738ac6f356061f1d022135d33d6
Jared Tesone <nyrfanjp3@gmail.com>
2019-04-02 21:11:28 -0400

    Added main attacks/behavior to boss fight
    Waiting for art/animations for more advance elements

--- GameDev2/Assets/FactoryBoss.cs
+++ GameDev2/Assets/FactoryBoss.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class FactoryBoss : MonoBehaviour
 {
     //public float detectRadius = 2.0f;
-    public float health_max = 2;
+    public float health_max = 20;
     private float health = 2;
+
+    //movement variables
+    public int mvmtForce = 10;
+    Vector2 mvdir = Vector2.left;
+    public float dist = 10f;
+    public float velocityCap = 10f;
+
+    //behavior management
+    int behavior = 0;   //start with default
+    float time = 0f;
+    float specTime = 0f;
+    float height;
+    GameObject player;
+    Player pscript;
+
+    //attack variables
+    public float swipeTime = 3f;
+    public float swipeDist = 10f;
+    public int swipeDamage = 1;
+
+    public float specialAttackTime = 15f;
+    public float stompTimer = 5f;
+
     public int attack = 1;
     public float seekSpeed = 1.25f;
     public float attackSpeed = 2f;
@@ -31,7 +54,6 @@ public class FactoryBoss : MonoBehaviour
     public Rigidbody2D rb;
     //public Collider2D detectCollider;
     //public Collider2D hitbox;
-    private GameObject player;
     private bool flash = false;
     public int flashRate = 3;
     private int flashCt = 0;
@@ -56,16 +78,135 @@ public class FactoryBoss : MonoBehaviour
 
     [SerializeField]
     //GameObject pa_playerObj;
-    Player pa_script;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get starting position
+        startPos = transform.position;
+        //get rigidbody and colliders
+        rb = GetComponent<Rigidbody2D>();
+        //detectCollider = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        //get player object
+        player = GameObject.FindGameObjectWithTag("Player");
+        pscript = player.GetComponent<Player>();
+        //enemSprite = GetComponent<Sprite>();
+        health = health_max;
+        rb.AddForce(Vector2.left * mvmtForce);
+        mvdir = Vector2.left;
+        height = sprite.bounds.extents.y;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject contact = collision.gameObject;
+        //Debug.Log(collision.gameObject);
+        if (contact.tag.Equals("Player"))
+        {
+            Debug.Log("I HIT THE PLAYER");
+            //anim.SetTrigger("Attack");
+            //Debug.Log("attack!");
+            Player pscript = contact.GetComponent<Player>();
+            //Debug.Log(lastDir);
+            if (!pscript.invincible)
+            {
+                pscript.anim.SetTrigger("Hurt");
+                pscript.takeDamage(attack, rb.velocity);
+            }
+        }
+        /*else if (contact.layer.Equals("Bullet"))
+        {
+            Debug.Log("bullet hit!");
+
+
+        }*/
+    }
+
+    void Swipe(Vector2 dir)
+    {
+        Debug.Log("swipe");
+        bool swipeRay = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height / 2), dir, swipeDist, LayerMask.GetMask("Player"));
+        Debug.DrawRay(new Vector2(transform.position.x, transform.position.y - height / 2), dir * swipeDist, Color.red);
+        if (swipeRay)
+        {
+            //player hit by swipe
+            Debug.Log("swiped the player");
+            pscript.takeDamage(swipeDamage, dir);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (specTime >= specialAttackTime)
+            behavior = -1;
+
+        if (behavior == 0)
+        {
+            Debug.Log(rb.velocity);
+            //move side-to-side using AddForce
+            //detect distance from wall
+            bool leftRay = Physics2D.Raycast(transform.position, Vector2.left, dist, LayerMask.GetMask("Ground"));
+            Debug.DrawRay(transform.position, Vector2.left * dist, Color.magenta);
+            if (leftRay)
+            {
+                //boss is within 10 units of left wall
+                //Debug.Log("left wall detected");
+                //switch to move right
+                mvdir = Vector2.right;
+            }
+            RaycastHit2D rightRay = Physics2D.Raycast(transform.position, Vector2.right, dist, LayerMask.GetMask("Ground"));
+            Debug.DrawRay(transform.position, Vector2.right * dist, Color.cyan);
+            if (rightRay)
+            {
+                //boss is within 10 units of right wall
+                //Debug.Log("right wall detected");
+                //switch to move left
+                mvdir = Vector2.left;
+            }
+            rb.AddForce(mvdir * mvmtForce);
+            if (rb.velocity.magnitude > velocityCap)
+                rb.velocity = mvdir * velocityCap;
+
+            //basic swipe attack
+            if (time >= swipeTime)
+            {
+                //cooldown over; swipe at player
+                Swipe(mvdir);
+                time = 0;
+            }
+            else
+                time += Time.deltaTime;
+        }
+        else if (behavior == 1)
+        {
+            //flight attack
+            Debug.Log("start flying");
+
+
+        } else if (behavior == 2)
+        {
+            //stomp attack
+            Debug.Log("Prepare to stomp");
+
+
+        } else
+        {
+            //randomly select special attack
+            if (Random.value >= 5.0f)
+            {
+                behavior = 1;
+
+            }
+            else
+            {
+                behavior = 2;
+                //stompTimer = 0;
+            }
+        }
+
+        specTime += Time.deltaTime;
     }
 }
