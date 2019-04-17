bc44a742b26a7254b33b409e5fed5f1a82388328
Alex Francoletti <alexander.francoletti@gmail.com>
2019-04-17 03:36:13 -0400

    Points added to UI

---
+++ GameDev2/Assets/Scripts/UpdatePower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdatePower : MonoBehaviour
+{
+    private Player pscript;
+    private string newText;
+    public TMP_Text tmp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pscript = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newText = "Power: " + pscript.points;
+        tmp.SetText(newText);
+    }
+}
