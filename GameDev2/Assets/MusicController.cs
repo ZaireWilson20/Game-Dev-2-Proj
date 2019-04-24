8046b014cab7cc3d9402d671e4c0cc9a9eda0819
Zaire Wilson <Zaire.R.Wilson@gmail.com>
2019-04-24 12:41:10 -0400

    Music wasn't loading

--- GameDev2/Assets/MusicController.cs
+++ GameDev2/Assets/MusicController.cs
@@ -10,6 +10,7 @@ public class MusicController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         musicEv = FMODUnity.RuntimeManager.CreateInstance(music);
         musicEv.start();
         
