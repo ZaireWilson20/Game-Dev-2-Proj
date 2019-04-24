39ebf99f0d673646ecb5f3b6b3f70645028c5590
Jared Tesone <nyrfanjp3@gmail.com>
2019-04-23 23:47:20 -0400

    Control Layout Screen from Pause Menu (sick)

---
+++ GameDev2/Assets/ControlDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDisplay : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Close()
+    {
+        menu.SetActive(true);
+        menu.GetComponentInChildren<PauseMenu>().backToMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Click"))
+        {
+            Close();
+        }
+    }
+}
